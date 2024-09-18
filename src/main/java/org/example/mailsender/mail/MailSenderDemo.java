7f301c3e7048259f9f1c41eb37472e7850219d62
HusanboyJorayev <144492116+HusanboyJorayev@users.noreply.github.com>
2024-09-18 16:35:48 +0500

    send file to email

--- src/main/java/org/example/mailsender/mail/MailSenderDemo.java
+++ src/main/java/org/example/mailsender/mail/MailSenderDemo.java
@@ -1,26 +1,32 @@
 package org.example.mailsender.mail;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashSet;
+import java.io.File;
+import java.util.Objects;
 import java.util.Random;
-import java.util.Set;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/mail")
+@Slf4j
 public class MailSenderDemo {
 
     private final JavaMailSender mailSender;
     private final UserRepository userRepository;
-    private final Set<String> set = new HashSet<>();
 
     @GetMapping("/sendMessage/")
     public ResponseEntity<?> sendSimpleMail(@RequestParam("to") String to) {
@@ -40,6 +46,35 @@ public class MailSenderDemo {
         return ResponseEntity.ok("MESSAGE SENT SUCCESSFULLY");
     }
 
+    @GetMapping("/sendAttachment/")
+    public ResponseEntity<?> sendSimpleMailWithAttachment(@RequestParam("to") String to,
+                                                          @RequestParam(value = "file", required = false) MultipartFile file) throws Exception {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+        Random random = new Random();
+        helper.setTo(to);
+        helper.setFrom("husanboybackanddeveloper@gmail.com");
+        helper.setSubject("HH.UZ");
+        int i = random.nextInt(1000, 9999);
+        helper.setText(Integer.toString(i));
+        User user = User.builder()
+                .email(to)
+                .code(Integer.toString(i))
+                .build();
+        userRepository.save(user);
+        if (file != null) {
+            File tempFile = File.createTempFile("upload-", Objects.requireNonNull(file.getOriginalFilename()));
+            file.transferTo(tempFile);
+
+            FileSystemResource resource = new FileSystemResource(tempFile);
+            helper.addAttachment(Objects.requireNonNull(resource.getFilename()), resource);
+            mailSender.send(mimeMessage);
+            tempFile.deleteOnExit();
+        } else
+            mailSender.send(mimeMessage);
+        return ResponseEntity.ok("MESSAGE SENT SUCCESSFULLY");
+    }
 
     @GetMapping("/success/")
     public ResponseEntity<?> success(@RequestParam("id") Integer id,
